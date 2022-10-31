b31c899bb6869096d491321947fbfaee53f6702d
lakshmi <LakshmiP@marsreturnship.com>
2022-10-31 09:51:54 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import ExpenseCalculator from './ExpenseCalculator';
 import sample from './sample';
 import GetName from './GetName';
+import Name from './Name';
 
 
 
@@ -9,7 +9,7 @@ import GetName from './GetName';
 function App() {
   return ( //jsx-html inside js
   <div className='App'>
-     <GetName/>
+     <Name/>
  </div>
   );
 }
